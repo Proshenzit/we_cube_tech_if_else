15415e401655e865e23023aa7ffa884e2ad03a95
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-08-26 21:11:15 -0700

    Add files via upload

---
+++ we_cube_tech_if_else.py
@@ -0,0 +1,11 @@
+per =int(input("Enter your value:-  "))
+if per >=60:
+    print("First Div")
+elif per >=48:
+    print("Second Div")
+
+elif per >=35:
+    print("Third Div")
+
+else:
+    print("Fail")
